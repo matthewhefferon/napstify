68c4ba3baa50cb78eda56857a9895fef97261cb2
Matthew Hefferon <matthefferon@gmail.com>
2025-08-31 10:28:24 -0600

    feat: add favicon and public assets

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,9 @@ import { Providers } from './providers'
 export const metadata: Metadata = {
   title: 'Napstify v2.0 BETA 6',
   description: 'Windows 98-style Spotify search interface',
+  icons: {
+    icon: '/favicon.svg',
+  },
 }
 
 export default function RootLayout({
